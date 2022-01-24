560303a284b451f5a448a7375e1c117dce91a691
Sebastian Proost <sebastian.proost@gmail.com>
2022-01-24 13:28:37 +0100

    cleaned code

--- generate_summary.py
+++ generate_summary.py
@@ -3,11 +3,21 @@ import pandas as pd
 from os import path
 
 from operations import groupby_country, summarize, groupby_location
+import click
 
-if __name__ == "__main__":
-    orders = sys.argv[1]
-    output_dir = sys.argv[2]
 
+@click.command()
+@click.argument('orders')
+@click.argument('output_dir')
+def run(orders, output_dir):
+    """
+    Will read a CSV file with parsed ORDERS and generate some summary statistics by location, country and total. The
+    results will be written to different files in OUTPUT_DIR
+
+
+    :param orders: path to CSV file with parsed orders
+    :param output_dir: directory to write summary statistics to
+    """
     # Read input
     processed_data = pd.read_csv(orders)
 
@@ -20,3 +30,9 @@ if __name__ == "__main__":
     by_location.to_csv(path.join(output_dir, "orders_by_location.csv"), index=None)
     by_country.to_csv(path.join(output_dir, "orders_by_country.csv"), index=None)
     totals.to_csv(path.join(output_dir, "orders_summary.csv"))
+
+
+if __name__ == "__main__":
+    run()
+
+
--- parse_folder.py
+++ parse_folder.py
@@ -6,6 +6,9 @@ import os
 from operations import add_new_shipments, start_pipeline, add_coordinates
 from dotenv import load_dotenv
 
+import click
+
+
 load_dotenv()  # take environment variables from .env.
 
 COLUMNS = [
@@ -31,20 +34,42 @@ def load_data(file: str) -> pd.DataFrame:
     return output
 
 
-if __name__ == "__main__":
-    api_key = os.environ[
-        "API_KEY"
-    ]  # Get API key from Google's Geocoding API from environment vars
+@click.command()
+@click.argument('orders') # , help="Path to CSV file with orders (a new file will be created if this doesn't exist)"
+@click.argument('eml_data') # , help="Path to directory with EML files with shipping requests"
+@click.option('--api_key', default=None, help='Google Geocoding API key')
+def run(orders, eml_data, api_key=None):
+    """
+    Will load previously parsed from ORDERS (or create a new file if these don't exist), and parse all eml files in
+    EML_DATA, geocode the results and add these.
 
-    orders = sys.argv[1]
-    eml_data = sys.argv[2]
+    :param orders: Path to an existing CSV file with previously parsed orders
+    :param eml_data: Path to directory with EML files to be processed
+    :param api_key: A Google API key which can access the Geocoding API
+    """
+    # Get API key from Google's Geocoding API from environment vars
+    if api_key is None:
+        api_key = os.environ[
+            "API_KEY"
+        ]
 
+    # # Get command line args
+    # orders = sys.argv[1]
+    # eml_data = sys.argv[2]
+
+    # Load orders (or create new df)
     processed_data = load_data(orders)
 
+    # Add new shipments (from eml files) and add coordinates
     processed_data = (
         processed_data.pipe(start_pipeline)
         .pipe(add_new_shipments, eml_data)
         .pipe(add_coordinates, api_key)
     )
 
+    # Write output
     processed_data.to_csv(orders, index=None)
+
+
+if __name__ == "__main__":
+    run()
